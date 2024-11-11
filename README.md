24317604ce478d7ff9e205b74c5bc46bb30a79eb
A-Slimani <abdullahslimani98@gmail.com>
2024-11-11 14:33:30 +1100

    completed scraping fighters

---
+++ README.md
@@ -0,0 +1,9 @@
+# README 
+This project is practice to create an end to end pipeline
+
+# TODO
+- Setup db
+    - Follow yt video
+    - With postgres docker
+- Create pipeline from scrapy to db
+- 
---
+++ ufcstats/ufcstats/items.py
@@ -0,0 +1,25 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class UfcstatsItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+class FighterItem(scrapy.Item):
+    first_name = scrapy.Field()
+    last_name = scrapy.Field()
+    nickname = scrapy.Field()
+    height = scrapy.Field()
+    weight = scrapy.Field()
+    reach = scrapy.Field()
+    stance = scrapy.Field()
+    wins = scrapy.Field()
+    losses = scrapy.Field()
+    draws = scrapy.Field()
+    belt = scrapy.Field()
